681aa6f2eab8c32e7bcf8992d981193c0b66810c
dfcantillo <diego.cantillo@iccs.es>
2024-02-18 18:55:33 -0500

    Preparción para subida a NPP

--- src/02-component-pattherns/pages/ShoppingPages.tsx
+++ src/02-component-pattherns/pages/ShoppingPages.tsx
@@ -1,5 +1,4 @@
 import { ProductButtons, ProductCard, ProductImage, ProductTitle } from "../components"
-import '../styles/custom-styles.css';
 import { products } from "../data/products";
 
 
@@ -16,22 +15,18 @@ export const ShoppingPages = () => {
       <h1>Shopping store</h1>
       <hr />
 
-      <ProductCard key={product.id} product={product} className="bg-dark text-bold"
+      <ProductCard key={product.id} product={product} 
         initialValues={{
           count: 2,
           maxCount: 10
         }}
       >
         {({reset,count,isMaxCountReached,increaseBy}) => (<>
-          <ProductImage className="custom-image" />
-          <ProductTitle className="text-white" />
-          <ProductButtons className="custom-button" />
+          <ProductImage  />
+          <ProductTitle  />
+          <ProductButtons  />
 
-          <button onClick={reset}>Reset</button>
-          {count>0 && <button onClick={()=>increaseBy(-2)}>-2</button>}
-          {!isMaxCountReached && <button onClick={()=>increaseBy(2)}>+2</button>}
-          <span style={{color:"white"}}>{count}</span>
-
+         
         </>)}
 
       </ProductCard>
